1880017f1830a33050fb8f99575ea91cb9b70203
Paul Datta <pkdatta2000@gmail.com>
2025-11-03 14:12:25 +0800

    feat: add setup script to clone petclinic repo and ignore it

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script clones the spring-petclinic repository.
+echo "Cloning spring-petclinic repository..."
+git clone https://github.com/spring-projects/spring-petclinic.git src-repo/spring-petclinic
